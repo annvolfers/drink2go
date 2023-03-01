fbe8849ef9b106ff7d3202bdfc60a8b2f08e4ac6
annvolfers <annvolfers@me.com>
2023-03-01 17:55:16 +0000

    Deploying to gh-pages from @ annvolfers/drink2go@8b9f2ca7ec96fc4f8a36904f8be6fa466f5a0d33 🚀

--- js/script.js
+++ js/script.js
@@ -1,1 +1,1 @@
-import"./mobile-menu.js";import"./slider.js";import"./sort.js";import"./pagination.js";import"./map.js";
+import"./mobile-menu.js";import"./slider.js";import"./range.js";import"./sort.js";import"./pagination.js";import"./map.js";
